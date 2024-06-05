5a923f9228ecab7983e0a0755a64d142f8aa3052
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-05 15:40:53 -0300

    Created the Category Model

--- contact/admin.py
+++ contact/admin.py
@@ -4,6 +4,12 @@ from contact import models
 # Register your models here.
 
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
+
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'first_name', 'last_name', 'phone', 'email'
--- contact/models.py
+++ contact/models.py
@@ -12,6 +12,16 @@ from django.utils import timezone
 # owner (foreign key)
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -21,6 +31,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
