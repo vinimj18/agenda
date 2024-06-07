96dfd4e4a1d6bd2edcd5b9cb5b56ecbbd6113d91
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-07 15:44:53 -0300

    Built search engine

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
 # type: ignore
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 # Create your views here.
@@ -14,6 +15,34 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contacts - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('first_name')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
