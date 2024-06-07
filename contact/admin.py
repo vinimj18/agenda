66305a2d1f2094c20a4b7ff805fb26f66b23e5bc
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-07 14:46:38 -0300

    Created single contact view and template

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,5 @@
+# type: ignore
+
 from django.contrib import admin
 from contact import models
 
@@ -12,11 +14,11 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'first_name', 'last_name', 'phone', 'email'
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'show',
     ordering = 'first_name',
     list_filter = 'created_date',
     list_per_page = 25
     list_max_show_all = 200
-    # list_editable = 'phone',
+    list_editable = 'phone', 'show',
     list_display_links = 'first_name', 'last_name',
     search_fields = 'id', 'first_name', 'last_name'
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+# type: ignore
+
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('first_name')[:10]
+
     context = {
         'contacts': contacts,
     }
@@ -13,5 +18,20 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
