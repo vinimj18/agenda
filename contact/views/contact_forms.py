800c771c5baa668da7b06b472dcc6050cc63d543
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-07 17:02:20 -0300

    Created URL, view, template for creating a contact

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
+from contact.models import Contact
+
+
+def create(request):
+    context = {}
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
