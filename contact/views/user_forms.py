13d3a95fda183d61c25ba84ea6c4fe4668348271
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-11 16:39:59 -0300

    Created login/logout system

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
@@ -10,6 +12,8 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'User created successfully')
+            return redirect('contact:index')
 
     return render(
         request,
@@ -18,3 +22,30 @@ def register(request):
             'form': form,
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('contact:index')
+        messages.error(request, 'Invalid user/password.')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
