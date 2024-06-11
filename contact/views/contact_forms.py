4b0cea0c23b9752889725fb1fad5d15f2e2dc3e3
vinimj18 <viniciusmaggiotto@gmail.com>
2024-06-11 14:45:45 -0300

    Created delete view, form and button

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from contact.forms import ContactForm
-from contact.models import Contact
+from contact.models import Contact  # type: ignore
 
 
 def create(request):
@@ -69,4 +69,22 @@ def update(request, contact_id):
         request,
         'contact/create.html',
         context,
+    )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        },
     )
